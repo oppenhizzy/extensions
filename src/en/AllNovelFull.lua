5f8e0980c18a5d1bcd0534279f3ce631ae1fd8d7
oppenhizzy <oppenhizzy@protonmail.com>
2021-11-19 14:06:05 -0500

    ...

--- src/en/AllNovelFull.lua
+++ src/en/AllNovelFull.lua
@@ -1,6 +1,6 @@
 -- {"id":1,"ver":"2.0.1","libVer":"1.0.0","author":"TechnoJo4","dep":["NovelFull>=2.0.2"]}
 
-return Require("NovelFull")("http://allnovelfull.com", {
+return Require("AllNovelFull")("http://allnovelfull.com", {
 	id = 1,
 	name = "AllNovelFull",
 	imageURL = "https://github.com/shosetsuorg/extensions/raw/dev/icons/NovelFull.png",
